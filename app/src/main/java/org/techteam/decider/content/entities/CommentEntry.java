a47adb3ed4a4267f5f741cb63df09e8c52b7d8e2
igorcoding <igorcoding@gmail.com>
2015-05-22 00:33:49 +0300

    Added CommentEntry

--- app/src/main/java/org/techteam/decider/content/entities/CommentEntry.java
+++ app/src/main/java/org/techteam/decider/content/entities/CommentEntry.java
@@ -1,22 +1,87 @@
 package org.techteam.decider.content.entities;
 
 import android.database.Cursor;
+import android.provider.BaseColumns;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Delete;
+import com.activeandroid.query.Select;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.techteam.decider.content.Entry;
 
-public class CommentEntry implements Entry {
+@Table(name = "Comment", id = BaseColumns._ID)
+public class CommentEntry extends Model {
+
+    @Column(name="cid", unique=true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    public int cid;
+
+    @Column(name="author", onUpdate = Column.ForeignKeyAction.CASCADE, onDelete = Column.ForeignKeyAction.CASCADE)
+    public UserEntry author;
+
+    @Column(name="q_text")
+    public String text;
+
+    @Column(name="creation_date")
+    public String creationDate; // TODO: change to DateTime
+
+    @Column(name="likes_count")
+    public int likesCount;
 
     protected CommentEntry() {
     }
 
-    public static CommentEntry fromJson(JSONObject obj) {
+    public int getCid() {
+        return cid;
+    }
 
-        return new CommentEntry();
+    public UserEntry getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public Long saveTotal() {
+        author.save();
+        return save();
+    }
+
+    public static CommentEntry byCId(int cid) {
+        return new Select().from(CommentEntry.class).where("cid = ?", cid).executeSingle();
+    }
+
+    public static CommentEntry fromJson(JSONObject obj) throws JSONException {
+        CommentEntry entry = new CommentEntry();
+        entry.cid = obj.getInt("id");
+        entry.text = obj.getString("text");
+        entry.creationDate = obj.getString("creation_date");
+        entry.author = UserEntry.fromJson(obj.getJSONObject("author"));
+        entry.likesCount = obj.getInt("likes_count");
+
+        return entry;
     }
 
     public static CommentEntry fromCursor(Cursor cursor) {
-        //TODO: fromCursor
-        return null;
+        CommentEntry entry = new CommentEntry();
+        entry.loadFromCursor(cursor);
+        return entry;
+    }
+
+    public static void deleteAll() {
+        new Delete().from(CommentEntry.class).execute();
     }
 }
