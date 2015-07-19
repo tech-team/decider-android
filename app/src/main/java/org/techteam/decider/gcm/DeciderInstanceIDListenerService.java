a7b6003425c1f523165b2ee66fb5bd6127171eb6
igorcoding <igorcoding@gmail.com>
2015-07-19 16:46:44 +0300

    Embedded Gcm sample app (working on #8)

--- app/src/main/java/org/techteam/decider/gcm/DeciderInstanceIDListenerService.java
+++ app/src/main/java/org/techteam/decider/gcm/DeciderInstanceIDListenerService.java
@@ -1,10 +1,20 @@
 package org.techteam.decider.gcm;
 
+import android.content.Intent;
+
 import com.google.android.gms.iid.InstanceIDListenerService;
 
 public class DeciderInstanceIDListenerService extends InstanceIDListenerService {
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
     @Override
     public void onTokenRefresh() {
-        super.onTokenRefresh();
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, GcmRegistrationIntentService.class);
+        startService(intent);
     }
 }
