6adb046203de6e3498855b3aa85e4d23ab61fbcf
alekseyl1992 <alekseyl@list.ru>
2015-07-23 22:58:57 +0300

    Spinner fix

--- app/src/main/java/org/techteam/decider/gui/views/PostView.java
+++ app/src/main/java/org/techteam/decider/gui/views/PostView.java
@@ -1,12 +1,10 @@
 package org.techteam.decider.gui.views;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public abstract class PostView extends FrameLayout {
